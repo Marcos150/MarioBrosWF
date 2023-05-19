999adca6ce00dce29a676d2248067819fac69593
Marcos Ruiz <marcosruizrubio@gmail.com>
2023-05-19 21:16:51 +0200

    Implementada la clase cangrejo de forma básica.

--- Cangrejo.cs
+++ Cangrejo.cs
@@ -1,5 +1,7 @@
 ﻿//Clase que define a los enemigos cangrejo
 
+using System.Drawing;
+
 namespace MarioBrosWF
 {
     internal class Cangrejo : Enemigo
@@ -8,8 +10,17 @@ namespace MarioBrosWF
 
         public Cangrejo() : base()
         {
+            this.FICHERO_SPRITE = "recursos/placeholder.png";
             this.estaEnfadado = false;
             vidas = 2;
+            spriteX = 0;
+            spriteY = 0;
+            imagen = Image.FromFile(FICHERO_SPRITE);
+            spriteVulnerable = FICHERO_SPRITE;
+            coordenadasX[DERECHA] = new int[] { 0, 0, 0, 0, 0 };
+            coordenadasY[DERECHA] = new int[] { 0, 0, 0, 0, 0 };
+            coordenadasX[IZQUIERDA] = new int[] { 0, 0, 0, 0, 0 };
+            coordenadasY[IZQUIERDA] = new int[] { 0, 0, 0, 0, 0 };
         }
     }
 }
